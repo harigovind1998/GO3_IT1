ac9f1dedf600e7fa40ef4bf34c798b427507aa06
Hari Govind <grhari1998@gmail.com>
2019-05-19 01:33:03 -0400

    Started setting up the server code. Added Function to the com class to generate any required message type, and moved the Check Message format function from the server class to ComFunctions so both the server, host and client can make use of it

--- src/Server.java
+++ src/Server.java
@@ -3,68 +3,27 @@ import java.net.DatagramSocket;
 import java.util.List;
 
 public class Server {
-	DatagramSocket recieveSocket, sendSocket;
-	DatagramPacket recievePacket, sendPacket;
+	private final int BLOCK_SIZE = 512;
+	DatagramSocket recieveSocket, errorSocket;
+	DatagramPacket recievePacket, errorPacket;
 	ComFunctions com;
-	
-	
-	/**
-	 * Checks to see if a byte array is on the required format read/write,text,0,text,0...0
-	 * @param msg message that is to be checked
-	 * @return true if the format is correct and false if it is not
-	 */
-	private boolean checkMessage(byte[] msg) {
-
-		if (msg[0] == 0 && (msg [1] == 1 || msg[1] == 2)) { //Check to see if the first 2 bytes specify a read or a write;
-			int count = 0; 
-			int i = 2;
-			//finds the first 2 0's
-			for (; i < msg.length; ++i) { 
-				if (msg[i] == 0 && (msg[i] != msg[i - 1])) {
-					++count;
-				}
-				if(count == 2) {
-					i++;
-					break;
-				}
-			}
-			
-			//checks to ensure the rest of the message are only 0's
-			for(;i<msg.length; i++) {
-				if(msg[i] != 0) {
-					return false;
-				}
-			}
-				return true;
-			
-		} else {
-			return false;
-		}
-		
-	}
 	
 	/**
 	 * loops and keeps serving all the incoming requests
 	 */
 	public void serve() {
 		while(true) {
-			//Recieves the incoming message and prints its contents
-			recievePacket = com.recievePacket(recieveSocket, com.UNKNOWNLEN);
-			com.printMessage("Recieved message from Host:", recievePacket.getData());
-			//handles the request approprioately 
-			if( checkMessage(recievePacket.getData())){
-				if(recievePacket.getData()[1]== 1){
-					sendPacket = com.createPacket(new byte[] {0,3,0,1},recievePacket.getPort());
-					com.printMessage("Sending message to Host:", sendPacket.getData());
-					com.sendPacket(sendPacket, sendSocket);
-				}else{
-					sendPacket = com.createPacket(new byte[] {0,4,0,0}, recievePacket.getPort());
-					com.printMessage("Sending message to Host:", sendPacket.getData());
-					com.sendPacket(sendPacket, sendSocket);
-				}
+			recievePacket = com.recievePacket(recieveSocket, BLOCK_SIZE);
+			if (com.checkMessage(recievePacket.getData())) {
+				ServerWorker worker = new ServerWorker(Integer.toString((recievePacket.getPort())), recievePacket);
+				worker.run();
 			}else {
-				throw new IllegalArgumentException();
+				errorPacket = com.createPacket(com.generateErrMessage(new byte[] {0, 0}, "The Read or write request was of invalid format"),recievePacket.getPort());
+				errorSocket = com.startSocket();
+				com.sendPacket(errorPacket, errorSocket);
+				errorSocket.close();
 			}
+			
 			
 			
 		}
@@ -73,9 +32,8 @@ public class Server {
 	public Server() {
 		// TODO Auto-generated constructor stub
 		com = new ComFunctions();
-		
 		recieveSocket = com.startSocket(69);
-		sendSocket = com.startSocket();		
+		recievePacket = com.createPacket(BLOCK_SIZE);
 		ServerExitListener exit = new ServerExitListener("Exit listener");
 		exit.run();
 	}
--- src/ServerWorker.java
+++ src/ServerWorker.java
@@ -3,6 +3,7 @@ import java.net.DatagramSocket;
 public class ServerWorker extends Thread {
 	private final int BLOCK_SIZE = 512;
 	private DatagramPacket initialPacket, RecievedResponse, SendingResponse;
+	private int clientPort;
 	private String fileName; 
 	private DatagramSocket SendRecieveSocket; 
 	private ComFunctions com;
@@ -13,11 +14,20 @@ public class ServerWorker extends Thread {
 		}
 	}
 	
+	public void run() {
+		
+		serve();
+		
+	}
+	
 	public ServerWorker(String name, DatagramPacket packet ) {
 		// TODO Auto-generated constructor stub
 		com = new  ComFunctions();
 		SendRecieveSocket = com.startSocket();
 		RecievedResponse = com.createPacket(BLOCK_SIZE);
+		initialPacket = packet;
+		clientPort  = packet.getPort();
+		
 		
 	}
 	
